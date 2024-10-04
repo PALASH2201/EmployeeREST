5c2792063402510e8e0bbef4136b9bceedf76c01
Prime-dev-group <66507422+Prime-dev-group@users.noreply.github.com>
2024-10-04 23:05:52 +0530

    using yaml file for test employees

--- src/main/java/com/example/demo/EmployeeRestAppApplication.java
+++ src/main/java/com/example/demo/EmployeeRestAppApplication.java
@@ -1,28 +1,26 @@
-package com.example.demo;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
-public class EmployeeRestAppApplication implements CommandLineRunner{
+public class EmployeeRestAppApplication implements CommandLineRunner {
 
-	@Autowired
-	EmployeeRepository employeeRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeRestAppApplication.class, args);
-		System.out.println("Running");
-	}
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		employeeRepository.save(new EmployeeRest("ABC","Devops",30000));
-		employeeRepository.save(new EmployeeRest("XYZ","Security",10000));
-		employeeRepository.save(new EmployeeRest("UVW","Programming",32000));
-		employeeRepository.save(new EmployeeRest("GHI","Security",22000));
-		System.out.println("Employees added by default");
-		
-	}
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private EmployeeConfig employeeConfig;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRestAppApplication.class, args);
+        System.out.println("Running");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Save employees defined in the configuration file
+        employeeRepository.saveAll(employeeConfig.getEmployees());
+        System.out.println("Employees added by default");
+    }
 }
